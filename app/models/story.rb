2562d23b6bf758b5b4b25169927e48fc2f6cafde
Julia Dickey <julia.dickey@gmail.com>
2016-11-03 10:42:00 -0700

    Add validations to stories

--- app/models/story.rb
+++ app/models/story.rb
@@ -2,4 +2,6 @@ class Story < ActiveRecord::Base
 
   has_attached_file :image, :styles => { :medium => "300x300>", :thumb => "100x100#" }, :default_url => "/images/:style/missing.png"
   validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
+  validates_presence_of :image, :title, :date
+
 end
