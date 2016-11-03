df11b46f981b0d7800ef300c24750804e75454bd
Julia Dickey <julia.dickey@gmail.com>
2016-11-03 14:48:08 -0700

    replace multiple placeholders with one default rose image

--- app/models/story.rb
+++ app/models/story.rb
@@ -1,7 +1,7 @@
 class Story < ActiveRecord::Base
 
 
-  has_attached_file :image, :styles => { :medium => "300x300>", :thumb => "100x100#" }
+  has_attached_file :image, :styles => { :medium => "300x300>", :thumb => "100x100#" }, :default_url => '1.jpg'
   validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
   validates_presence_of :title, :date
 
